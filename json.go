6d23eb7e09ef3c9ec736353a68910dbc93bf6443
Alex Suraci <suraci.alex@gmail.com>
2021-08-17 14:15:16 -0400

    add NewEncoder and NewDecoder
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- json.go
+++ json.go
@@ -3,22 +3,29 @@ package bass
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 )
 
-func UnmarshalJSON(payload []byte, dest interface{}) error {
-	buf := bytes.NewBuffer(payload)
-
-	dec := json.NewDecoder(buf)
+func NewDecoder(r io.Reader) *json.Decoder {
+	dec := json.NewDecoder(r)
 	dec.UseNumber()
-	return dec.Decode(dest)
+	return dec
+}
+
+func NewEncoder(w io.Writer) *json.Encoder {
+	enc := json.NewEncoder(w)
+	enc.SetEscapeHTML(false)
+	return enc
+}
+
+func UnmarshalJSON(payload []byte, dest interface{}) error {
+	return NewDecoder(bytes.NewBuffer(payload)).Decode(dest)
 }
 
 func MarshalJSON(val interface{}) ([]byte, error) {
 	buf := new(bytes.Buffer)
 
-	enc := json.NewEncoder(buf)
-	enc.SetEscapeHTML(false)
-	err := enc.Encode(val)
+	err := NewEncoder(buf).Encode(val)
 	if err != nil {
 		return nil, err
 	}
