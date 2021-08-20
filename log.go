413dc022324ec3a9bb22aa1d2bf7ab8cf5a39a24
Alex Suraci <suraci.alex@gmail.com>
2021-08-19 22:43:47 -0400

    add LoggerTo
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- log.go
+++ log.go
@@ -10,7 +10,7 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-func Logger() *zap.Logger {
+func LoggerTo(w io.Writer) *zap.Logger {
 	zapcfg := zap.NewDevelopmentEncoderConfig()
 	zapcfg.EncodeLevel = zapcore.LowercaseColorLevelEncoder
 	zapcfg.EncodeTime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
@@ -19,9 +19,13 @@ func Logger() *zap.Logger {
 
 	return zap.New(zapcore.NewCore(
 		zapcore.NewConsoleEncoder(zapcfg),
-		zapcore.AddSync(colorable.NewColorableStderr()),
+		zapcore.AddSync(w),
 		zapcore.DebugLevel,
 	))
+}
+
+func Logger() *zap.Logger {
+	return LoggerTo(colorable.NewColorableStderr())
 }
 
 func Dump(dst io.Writer, val interface{}) {
