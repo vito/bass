810410faa71a5489d6e0a1b2ddc112455b24ad26
Alex Suraci <suraci.alex@gmail.com>
2021-09-12 14:58:02 -0400

    emit progress for bass -e too
    sometimes things don't build until they're exported (i.e. a
    constructed-but-never-run workload path), so we need to show progress
    here too

---
+++ cmd/bass/progress.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/vito/bass"
+	"github.com/vito/bass/ioctx"
+	"github.com/vito/bass/zapctx"
+	"github.com/vito/progrock"
+	"golang.org/x/sync/errgroup"
+)
+
+func withProgress(ctx context.Context, f func(context.Context, *progrock.Recorder) error) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt)
+	defer stop()
+
+	recorder := progrock.NewRecorder()
+	ctx = progrock.RecorderToContext(ctx, recorder)
+
+	eg := new(errgroup.Group)
+
+	eg.Go(func() error {
+		// start reading progress so we can initialize the logging vertex
+		return recorder.Display("Playing", os.Stderr)
+	})
+
+	// go!
+	eg.Go(func() (err error) {
+		defer recorder.Close()
+
+		logVertex := recorder.Vertex("log", "[bass]")
+		defer func() { logVertex.Done(err) }()
+
+		stderr := logVertex.Stderr()
+
+		// wire up logs to vertex
+		logger := bass.LoggerTo(stderr)
+		ctx = zapctx.ToContext(ctx, logger)
+
+		// wire up stderr for (log), (debug), etc.
+		ctx = ioctx.StderrToContext(ctx, stderr)
+
+		err = f(ctx, recorder)
+		if err != nil {
+			bass.WriteError(ctx, stderr, err)
+			return err
+		}
+
+		return
+	})
+
+	return eg.Wait()
+}
--- cmd/bass/run.go
+++ cmd/bass/run.go
@@ -3,19 +3,12 @@ package main
 import (
 	"context"
 	"os"
-	"os/signal"
 
 	"github.com/vito/bass"
-	"github.com/vito/bass/ioctx"
-	"github.com/vito/bass/zapctx"
 	"github.com/vito/progrock"
-	"golang.org/x/sync/errgroup"
 )
 
 func run(ctx context.Context, scope *bass.Scope, filePath string) error {
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt)
-	defer stop()
-
 	file, err := os.Open(filePath)
 	if err != nil {
 		return err
@@ -23,40 +16,8 @@ func run(ctx context.Context, scope *bass.Scope, filePath string) error {
 
 	defer file.Close()
 
-	recorder := progrock.NewRecorder()
-	ctx = progrock.RecorderToContext(ctx, recorder)
-
-	eg := new(errgroup.Group)
-
-	eg.Go(func() error {
-		// start reading progress so we can initialize the logging vertex
-		return recorder.Display("Playing", os.Stderr)
+	return withProgress(ctx, func(ctx context.Context, recorder *progrock.Recorder) error {
+		_, err := bass.EvalReader(ctx, scope, file)
+		return err
 	})
-
-	// go!
-	eg.Go(func() (err error) {
-		defer recorder.Close()
-
-		logVertex := recorder.Vertex("log", "[bass]")
-		defer func() { logVertex.Done(err) }()
-
-		stderr := logVertex.Stderr()
-
-		// wire up logs to vertex
-		logger := bass.LoggerTo(stderr)
-		ctx = zapctx.ToContext(ctx, logger)
-
-		// wire up stderr for (log), (debug), etc.
-		ctx = ioctx.StderrToContext(ctx, stderr)
-
-		_, err = bass.EvalReader(ctx, scope, file)
-		if err != nil {
-			bass.WriteError(ctx, stderr, err)
-			return err
-		}
-
-		return nil
-	})
-
-	return eg.Wait()
 }
