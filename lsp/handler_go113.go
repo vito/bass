ff7611a44fc5d1c43e212db6f33ff1c78923a83e
Alex Suraci <suraci.alex@gmail.com>
2021-09-18 01:12:05 -0400

    assimilate lsp subtree into bass go module
    (and clean up things that aren't needed)

---
+++ lsp/handler_go113.go
@@ -0,0 +1,14 @@
+// +build go1.13
+
+package lsp
+
+import (
+	"os/exec"
+)
+
+func succeeded(err error) bool {
+	exitErr, ok := err.(*exec.ExitError)
+	// When the context is canceled, the process is killed,
+	// and the exit code is -1
+	return ok && exitErr.ExitCode() < 0
+}
