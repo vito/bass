1190fac4ced0cd13fe8d22f2814ee50ab35466e9
Alex Suraci <suraci.alex@gmail.com>
2021-09-26 20:26:34 -0400

    better handling for progress with piped basses
    previously, piping bass scripts like ./ci/get | ./ci/build would barf
    all over the terminal, with both scripts attempting to display the
    progress UI at the same time.
    
    now, bass processes within a process group will select one of them to be
    the one that actually prints, with the rest connecting to it and sending
    progress over RPC.
    
    known issue: leaks socket files under ~/.cache/bass/recorder.<pgid>.sock

--- cmd/bass/progress.go
+++ cmd/bass/progress.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"net"
 	"os"
 	"os/signal"
+	"syscall"
 
+	"github.com/adrg/xdg"
 	"github.com/vito/bass"
 	"github.com/vito/bass/ioctx"
 	"github.com/vito/bass/zapctx"
 	"github.com/vito/progrock"
+	"github.com/vito/progrock/ui"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -16,24 +21,30 @@ func withProgress(ctx context.Context, f func(context.Context, *progrock.Recorde
 	ctx, stop := signal.NotifyContext(ctx, os.Interrupt)
 	defer stop()
 
-	recorder := progrock.NewRecorder()
+	statuses, recorder, err := electRecorder(ctx)
+	if err != nil {
+		return err
+	}
+
 	ctx = progrock.RecorderToContext(ctx, recorder)
 
 	eg := new(errgroup.Group)
 
-	eg.Go(func() error {
-		// start reading progress so we can initialize the logging vertex
-		return recorder.Display("Playing", os.Stderr)
-	})
+	if statuses != nil {
+		eg.Go(func() error {
+			// start reading progress so we can initialize the logging vertex
+			return ui.Display("Playing", os.Stderr, statuses)
+		})
+	}
 
 	// go!
 	eg.Go(func() (err error) {
 		defer recorder.Close()
 
-		logVertex := recorder.Vertex("log", "[bass]")
-		defer func() { logVertex.Done(err) }()
+		bassVertex := recorder.Vertex("bass", "[bass]")
+		defer func() { bassVertex.Done(err) }()
 
-		stderr := logVertex.Stderr()
+		stderr := bassVertex.Stderr()
 
 		// wire up logs to vertex
 		logger := bass.LoggerTo(stderr)
@@ -52,4 +63,23 @@ func withProgress(ctx context.Context, f func(context.Context, *progrock.Recorde
 	})
 
 	return eg.Wait()
+}
+
+func electRecorder(ctx context.Context) (ui.Reader, *progrock.Recorder, error) {
+	socketPath, err := xdg.CacheFile(fmt.Sprintf("bass/recorder.%d.sock", syscall.Getpgrp()))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var r ui.Reader
+	var w progrock.Writer
+	l, err := net.Listen("unix", socketPath)
+	if err != nil {
+		r = nil // don't display any progress; send it to the leader
+		w, err = progrock.DialRPC("unix", socketPath)
+	} else {
+		r, w, err = progrock.ServeRPC(l)
+	}
+
+	return r, progrock.NewRecorder(w), err
 }
