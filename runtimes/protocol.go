8307634b09d98c98ee9e36b887d6e57ddef4ba3c
Alex Suraci <suraci.alex@gmail.com>
2021-11-21 03:48:08 -0500

    add unix-table protocol for parsing simple clis

--- runtimes/protocol.go
+++ runtimes/protocol.go
@@ -1,9 +1,11 @@
 package runtimes
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/vito/bass/ghcmd"
 )
@@ -36,6 +38,67 @@ var Protocols = map[string]Protocol{
 
 	"json":          JSONProtocol{},
 	"github-action": GitHubActionProtocol{},
+	"unix-table":    UnixTableProtocol{},
+}
+
+// UnixTableProtocol parses lines of tabular output with columns separated by
+// whitespace. It emits JSON encoded arrays containing each row's columns.
+//
+// Each row is not guaranteed to have the same number of columns. Empty lines
+// correspond to empty arrays.
+type UnixTableProtocol struct{}
+
+// ResponseWriter returns res with a no-op Flush.
+func (UnixTableProtocol) ResponseWriter(res io.Writer, _ LogWriter) ProtoWriter {
+	return &unixTableWriter{
+		enc: json.NewEncoder(res),
+	}
+}
+
+type unixTableWriter struct {
+	enc *json.Encoder
+	buf []byte
+}
+
+func (w *unixTableWriter) Write(p []byte) (int, error) {
+	written := len(p)
+
+	for len(p) > 0 {
+		if w.buf != nil {
+			cp := []byte{}
+			cp = append(cp, w.buf...)
+			cp = append(cp, p...)
+			p = cp
+			w.buf = nil
+		}
+
+		ln := bytes.IndexRune(p, '\n')
+		if ln == -1 {
+			cp := []byte{}
+			cp = append(cp, p...)
+			w.buf = cp
+			break
+		}
+
+		row := string(p[:ln])
+
+		err := w.enc.Encode(strings.Fields(row))
+		if err != nil {
+			return 0, err
+		}
+
+		p = p[ln+1:]
+	}
+
+	return written, nil
+}
+
+func (w unixTableWriter) Flush() error {
+	if len(w.buf) > 0 {
+		return w.enc.Encode(strings.Fields(string(w.buf)))
+	}
+
+	return nil
 }
 
 // NewProtoWriter constructs a ProtoWriter for handling the named protocol.
