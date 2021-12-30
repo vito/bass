610f7ae8ced681810b37e1b271311cea8cb10029
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-12-30 03:34:43 +0200

    Make locating buildkit easier

--- default.nix
+++ default.nix
@@ -1,4 +1,8 @@
-{ lib, buildGoModule }:
+{ lib
+, buildGoModule
+, makeWrapper
+, buildkit
+}:
 
 buildGoModule rec {
   pname = "bass";
@@ -8,9 +12,16 @@ buildGoModule rec {
   # get using ./hack/get-nix-vendorsha
   vendorSha256 = "sha256-YauCD97LRd649ag/EQ8VrNc90oXszk/brBcj33mvjrM=";
 
+  nativeBuildInputs = [ makeWrapper ];
+
   ldflags = [
     "-X github.com/vito/bass.Version=${version}"
   ];
+
+  postInstall = ''
+    wrapProgram $out/bin/bass \
+      --prefix PATH : ${lib.makeBinPath [ buildkit ]}
+  '';
 
   subPackages = [ "cmd/bass" "cmd/bass-lsp" ];
 }
