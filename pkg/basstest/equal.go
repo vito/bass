801eebe9fad8acfcb6f498f163c383341d3e978d
Alex Suraci <suraci.alex@gmail.com>
2022-01-22 18:32:29 -0500

    prevent cmp panic

--- pkg/basstest/equal.go
+++ pkg/basstest/equal.go
@@ -1,6 +1,7 @@
 package basstest
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -11,8 +12,20 @@ func Equal(t *testing.T, a, b bass.Value) {
 	t.Helper()
 
 	if !a.Equal(b) {
-		diff := cmp.Diff(a, b)
+		diff := tryDiff(a, b)
 		t.Logf("%s != %s\n%s", a, b, diff)
 		t.FailNow()
 	}
+}
+
+func tryDiff(a, b interface{}) (res string) {
+	defer func() {
+		// cmp panics if equal is asymmetrical; recover for better failure ux
+		err := recover()
+		if err != nil {
+			res = fmt.Sprintf("diff error: %s", err)
+		}
+	}()
+
+	return cmp.Diff(a, b)
 }
