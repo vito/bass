f9ec199a8d371da29a67a65514deedf55fd8a3a7
Alex Suraci <suraci.alex@gmail.com>
2022-07-19 15:31:25 -0400

    fix exporting thunk images

--- cmd/bass/export.go
+++ cmd/bass/export.go
@@ -32,19 +32,7 @@ func export(ctx context.Context) error {
 		var path bass.ThunkPath
 		err = json.Unmarshal([]byte(msg), &path)
 		if err == nil {
-			platform := path.Thunk.Platform()
-			if platform == nil {
-				return fmt.Errorf("cannot export bass thunk path: %s", path)
-			}
-
-			runtime, err := bass.RuntimeFromContext(ctx, *platform)
-			if err != nil {
-				return err
-			}
-
-			return writeTar(vertex, func(w io.Writer) error {
-				return runtime.ExportPath(ctx, w, path)
-			})
+			return exportPath(ctx, vertex, path)
 		} else {
 			errs = multierror.Append(errs, err)
 		}
@@ -52,24 +40,44 @@ func export(ctx context.Context) error {
 		var thunk bass.Thunk
 		err = json.Unmarshal([]byte(msg), &thunk)
 		if err == nil {
-			platform := path.Thunk.Platform()
-			if platform == nil {
-				return fmt.Errorf("cannot export bass thunk: %s", thunk)
-			}
-
-			runtime, err := bass.RuntimeFromContext(ctx, *platform)
-			if err != nil {
-				return err
-			}
-
-			return writeTar(vertex, func(w io.Writer) error {
-				return runtime.Export(ctx, w, thunk)
-			})
+			return exportThunk(ctx, vertex, thunk)
 		} else {
 			errs = multierror.Append(errs, err)
 		}
 
 		return fmt.Errorf("unknown payload; must be a thunk or thunk path\n%w", errs)
+	})
+}
+
+func exportPath(ctx context.Context, vertex *progrock.VertexRecorder, path bass.ThunkPath) error {
+	platform := path.Thunk.Platform()
+	if platform == nil {
+		return fmt.Errorf("cannot export bass thunk path: %s", path)
+	}
+
+	runtime, err := bass.RuntimeFromContext(ctx, *platform)
+	if err != nil {
+		return err
+	}
+
+	return writeTar(vertex, func(w io.Writer) error {
+		return runtime.ExportPath(ctx, w, path)
+	})
+}
+
+func exportThunk(ctx context.Context, vertex *progrock.VertexRecorder, thunk bass.Thunk) error {
+	platform := thunk.Platform()
+	if platform == nil {
+		return fmt.Errorf("cannot export bass thunk: %s", thunk)
+	}
+
+	runtime, err := bass.RuntimeFromContext(ctx, *platform)
+	if err != nil {
+		return err
+	}
+
+	return writeTar(vertex, func(w io.Writer) error {
+		return runtime.Export(ctx, w, thunk)
 	})
 }
 
