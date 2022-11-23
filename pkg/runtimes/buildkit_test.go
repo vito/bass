b657f83fbaeb92f7984b517ce9c16f1e409f3256
Alex Suraci <suraci.alex@gmail.com>
2022-11-22 23:58:27 -0500

    just use the outer tls config

--- pkg/runtimes/buildkit_test.go
+++ pkg/runtimes/buildkit_test.go
@@ -5,15 +5,11 @@ import (
 	"testing"
 
 	"github.com/vito/bass/pkg/bass"
-	"github.com/vito/bass/pkg/basstls"
 	"github.com/vito/bass/pkg/runtimes"
 	"github.com/vito/bass/pkg/runtimes/util/buildkitd"
-	"github.com/vito/is"
 )
 
 func TestBuildkitRuntime(t *testing.T) {
-	is := is.New(t)
-
 	if testing.Short() {
 		t.SkipNow()
 		return
@@ -23,16 +19,12 @@ func TestBuildkitRuntime(t *testing.T) {
 
 	buildkitd.Remove(context.Background(), testInst)
 
-	tls := t.TempDir()
-	is.NoErr(basstls.Init(tls))
-
 	runtimes.Suite(t, bass.RuntimeConfig{
 		Platform: bass.LinuxPlatform,
 		Runtime:  runtimes.BuildkitName,
 		Config: bass.Bindings{
 			"debug":        bass.Bool(true),
 			"installation": bass.String(testInst),
-			"certs_dir":    bass.String(tls),
 		}.Scope(),
 	})
 }
