6481cec74dab266e38b500a341f2dc902f6fb6e0
Alex Suraci <suraci.alex@gmail.com>
2022-11-23 18:14:25 -0500

    configure tls depot for tests

--- pkg/runtimes/buildkit_test.go
+++ pkg/runtimes/buildkit_test.go
@@ -2,6 +2,7 @@ package runtimes_test
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/vito/bass/pkg/bass"
@@ -19,12 +20,18 @@ func TestBuildkitRuntime(t *testing.T) {
 
 	buildkitd.Remove(context.Background(), testInst)
 
+	config := bass.Bindings{
+		"debug":        bass.Bool(true),
+		"installation": bass.String(testInst),
+	}
+
+	if dir, ok := os.LookupEnv("BASS_TLS_DEPOT"); ok && dir != "" {
+		config["certs_dir"] = bass.String(dir)
+	}
+
 	runtimes.Suite(t, bass.RuntimeConfig{
 		Platform: bass.LinuxPlatform,
 		Runtime:  runtimes.BuildkitName,
-		Config: bass.Bindings{
-			"debug":        bass.Bool(true),
-			"installation": bass.String(testInst),
-		}.Scope(),
+		Config:   config.Scope(),
 	})
 }
